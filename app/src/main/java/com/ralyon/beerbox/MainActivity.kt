8c41202d8d1f4c2983758ec95a55a4d694673dce
ralyon <riccardo.pinferetti@gmail.com>
2023-04-06 21:13:32 +0200

    fix missing name attribute in app AndroidManifest.xml add Accompanist SystemUiController dependency to app module set system bars colors in MainActivity.kt

--- app/src/main/java/com/ralyon/beerbox/MainActivity.kt
+++ app/src/main/java/com/ralyon/beerbox/MainActivity.kt
@@ -3,14 +3,21 @@ package com.ralyon.beerbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ralyon.beerbox.ui.theme.BeerBoxTheme
+import com.ralyon.beerbox.ui.theme.DarkBackground
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,28 +25,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Set the system bars color
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = isSystemInDarkTheme()
+            SideEffect {
+                systemUiController.setSystemBarsColor(
+                    color = DarkBackground,
+                    darkIcons = useDarkIcons
+                )
+            }
+
             BeerBoxTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = "Beer Box",
+                        color = Color.White,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(16.dp)
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BeerBoxTheme {
-        Greeting("Android")
     }
 }
